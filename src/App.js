8c7acab8021a2a85139f98429855c8379503fc9d
abzel <32515091+abzel@users.noreply.github.com>
2021-04-02 18:23:33 +0600

    initial commit for master

--- src/App.js
+++ src/App.js
@@ -1,41 +1,8 @@
-import { Card } from "./components/Card";
-import "./App.css";
-import pic from './pic.png'
+// import "./index.css";
+import {Counter} from './components/Counter';
 
-function App() {
-  const data = [
-    {
-      title: "Physical Health",
-      color: "#22356F",
-    },
-    {
-      title: "Mental Health",
-      color: "#0052C1",
-    },
-    {
-      title: "Nutrition",
-      color: null,
-      img: pic
-    },
-    {
-      title: "Gymnastics",
-      color: "#62D0DF",
-    },
-    {
-      title: "Crossfit",
-      color: "#DEE1FF",
-    },
-    {
-      title: "Aerobics",
-      color: "#8F00FF",
-    },
-  ];
-  return (
-    <div className="App">
-      {data.map((el, id) => {
-        return <Card data={el} key={id} />;
-      })}
-    </div>
-  );
-}
-export default App;
+export const App = () => {
+  return <div className="App">
+    <Counter/>
+  </div>;
+};
