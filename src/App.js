35598cc0d42a95e2fe7bee2d91f2f902d2bf9f46
abzel <32515091+abzel@users.noreply.github.com>
2021-04-02 18:32:46 +0600

    modified App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-// import "./index.css";
-import {Counter} from './components/Counter';
 
-export const App = () => {
+
+function App() {
   return <div className="App">
-    <Counter/>
+    Master
   </div>;
 };
